fc817074b9f1e73affdc3a8154f169170fdc3036
JJoan <1720152558@qq.com>
2022-12-04 19:00:17 +0800

    feat: add scroll

--- src/pages/menu/components/list/index.jsx
+++ src/pages/menu/components/list/index.jsx
@@ -1,50 +1,123 @@
-import React, { useState, useCallback } from 'react';
-import { View, Text, Image } from '@tarojs/components';
+import React, { useState, useCallback, useMemo, useRef, useEffect } from 'react';
+import { View, Text, Image, ScrollView, Swiper, SwiperItem } from '@tarojs/components';
 import { AtIcon, AtTabsPane } from 'taro-ui';
 import styles from './index.module.scss';
 import { mockMenu, mockList } from './mockData';
 
 const List = () => {
-  const [current, setCurrent] = useState(0);
+  // const [scrollIntoViewKey, setScrollIntoViewKey] = useState(0);
+  const [activeKey, setActiveKey] = useState(0);
   const [menuList, setMenuList] = useState(mockMenu);
   const [list, setList] = useState(mockList);
+  const [scrollTop, setScrollTop] = useState(0);
+  const itemRef = useRef();
 
-  const handleClick = useCallback((v) => {
-    setCurrent(v);
+  const heightBoundaryList = useMemo(() => {
+    const heightList = [];
+    const cardHeight = 64;
+    const ItemHeight = 99;
+    const marginHeight = 11;
+    let sumHeight = 0;
+
+    list.forEach((item) => {
+      // console.log(111, item);
+      const currentHeight = cardHeight + ItemHeight * (item.length - 1) + marginHeight;
+      sumHeight += currentHeight;
+      heightList.push(sumHeight);
+    });
+
+    // console.log(222, heightList);
+
+    return heightList;
+  }, [list]);
+
+  const onScroll = useCallback(
+    ({ detail }) => {
+      const { scrollTop: top } = detail;
+      const index = heightBoundaryList.findIndex((item) => top < item);
+      console.log(top, heightBoundaryList);
+
+      setActiveKey(index);
+    },
+    [heightBoundaryList],
+  );
+
+  useEffect(() => {
+    console.log('headerRef', itemRef);
   }, []);
 
   return (
     <View className={styles.wrap}>
-      <View className={styles.left}>
+      {/* 左侧菜单 */}
+      <ScrollView className={styles.left} showScrollbar={false}>
         {menuList.map((item, index) => (
-          <View className={styles.leftItem} key={index}>
+          <View
+            className={`${styles.leftItem} ${activeKey === item.id ? styles.activeMenu : ''}`}
+            key={index}
+            onClick={() => {
+              const height = index - 1 >= 0 ? heightBoundaryList[index - 1] : 0;
+              setScrollTop(height);
+            }}
+          >
             <View className={styles.leftContent}>{item.title}</View>
           </View>
         ))}
-      </View>
-      <View className={styles.right}>
-        <View className={styles.rightContent}>
-          {list.map((item, index) => (
-            <View className={styles.rightItem} key={index}>
-              <Image src={item.imageUrl} className={styles.image} />
-              <View className={styles.info}>
-                <View>
-                  <Text className={styles.title}>{item.title}</Text>
-                </View>
-                <View style={{ margin: '2px 0' }}>
-                  <Text className={styles.desc}>{item.desc}</Text>
-                </View>
-                <View>
-                  <Text className={styles.originalPrice}>¥{item.originalPrice}&nbsp;&nbsp;</Text>
-                  <Text className={styles.discountPrice}>¥{item.discountPrice}</Text>
-                </View>
-                {/* <Text className={styles.handPrice}>{item.handPrice}</Text> */}
+      </ScrollView>
+
+      {/* 右侧菜单 */}
+      <ScrollView
+        className={styles.right}
+        scrollY
+        // 在设置滚动条位置时使用动画过渡
+        scrollWithAnimation
+        // 设置竖向滚动条位置
+        scrollTop={scrollTop}
+        // 距底部多远时（单位px），触发 scrolltolower 事件
+        // lowerThreshold={Threshold}
+        // 距顶部多远时（单位px），触发 scrolltoupper 事件
+        // upperThreshold={Threshold}
+        // onScrollToLower={onScrollToLower}
+        // 滚动到顶部，会触发 scrolltoupper 事件
+        // onScrollToUpper={onScrollToUpper}
+        onScroll={onScroll}
+        // 值应为某子元素id（id不能以数字开头）。设置哪个方向可滚动，则在哪个方向滚动到该元素
+        // scrollIntoView={{ scrollIntoViewKey }}
+        // showScrollbar={false}
+      >
+        {list.map((categoryItem, idx) => {
+          const cardItem = categoryItem[0];
+          const itemList = categoryItem.slice(1);
+          return (
+            <View key={idx} className={styles.rightItem} ref={itemRef}>
+              <View className={styles.card} id={`card_${cardItem.id}`}>
+                <View>{cardItem.title}</View>
+                <View className={styles.cardSubTitle}>{cardItem.subTitle}</View>
               </View>
-              <View className={`at-icon at-icon-add ${styles.icon}`}></View>
+
+              {itemList.map((item, index) => (
+                <View className={styles.coffeeItem} key={index} id={`product_${item.id}`}>
+                  <Image src={item.imageUrl} className={styles.image} />
+                  <View className={styles.info}>
+                    <View>
+                      <Text className={styles.title}>{item.title}</Text>
+                    </View>
+                    <View style={{ margin: '2px 0' }}>
+                      <Text className={styles.desc}>{item.desc}</Text>
+                    </View>
+                    <View>
+                      <Text className={styles.originalPrice}>
+                        ¥{item.originalPrice}&nbsp;&nbsp;
+                      </Text>
+                      <Text className={styles.discountPrice}>¥{item.discountPrice}</Text>
+                    </View>
+                  </View>
+                  <View className={`at-icon at-icon-add ${styles.icon}`}></View>
+                </View>
+              ))}
             </View>
-          ))}
-        </View>
-      </View>
+          );
+        })}
+      </ScrollView>
     </View>
   );
 };
--- src/pages/menu/components/list/mockData.js
+++ src/pages/menu/components/list/mockData.js
@@ -1,73 +1,202 @@
 import coffeePng from '../../../../assets/images/coffee.png';
 
 export const mockMenu = [
-  { title: 'Dirty系列', id: 0 },
-  { title: '生酪拿铁', id: 2 },
-  { title: '丝绒拿铁', id: 3 },
-  { title: '生椰家族', id: 4 },
-  { title: '小黑杯', id: 5 },
-  { title: '瑞纳冰', id: 6 },
-  { title: '经典拿铁', id: 7 },
-  { title: '大师咖啡', id: 8 },
-  { title: '不喝咖啡', id: 9 },
-  { title: '甜品小点', id: 10 },
+  { title: '人气Top', id: 0 },
+  { title: '生酪拿铁', id: 1 },
+  { title: '丝绒拿铁', id: 2 },
+  // { title: '生椰家族', id: 4 },
+  // { title: '小黑杯', id: 5 },
+  // { title: '瑞纳冰', id: 6 },
+  // { title: '经典拿铁', id: 7 },
+  // { title: '大师咖啡', id: 8 },
+  // { title: '不喝咖啡', id: 9 },
+  // { title: '甜品小点', id: 10 },
 ];
 
 export const mockList = [
-  {
-    title: 'Dirty系列',
-    desc: '2022拿铁冠军',
-    originalPrice: 32,
-    discountPrice: 28,
-    handPrice: 15,
-    imageUrl: coffeePng,
-  },
-  {
-    title: 'Dirty系列',
-    desc: '2022拿铁冠军',
-    originalPrice: 32,
-    discountPrice: 28,
-    handPrice: 15,
-    imageUrl: coffeePng,
-  },
-  {
-    title: 'Dirty系列',
-    desc: '2022拿铁冠军',
-    originalPrice: 32,
-    discountPrice: 28,
-    handPrice: 15,
-    imageUrl: coffeePng,
-  },
-  {
-    title: 'Dirty系列',
-    desc: '2022拿铁冠军',
-    originalPrice: 32,
-    discountPrice: 28,
-    handPrice: 15,
-    imageUrl: coffeePng,
-  },
-  {
-    title: 'Dirty系列',
-    desc: '2022拿铁冠军',
-    originalPrice: 32,
-    discountPrice: 28,
-    handPrice: 15,
-    imageUrl: coffeePng,
-  },
-  {
-    title: 'Dirty系列',
-    desc: '2022拿铁冠军',
-    originalPrice: 32,
-    discountPrice: 28,
-    handPrice: 15,
-    imageUrl: coffeePng,
-  },
-  {
-    title: 'Dirty系列',
-    desc: '2022拿铁冠军',
-    originalPrice: 32,
-    discountPrice: 28,
-    handPrice: 15,
-    imageUrl: coffeePng,
-  },
+  // 人气Top
+  [
+    {
+      id: 0,
+      type: 'card',
+      title: '人气Top',
+      subTitle: '必喝爆款，瑞幸精品',
+    },
+    {
+      id: '0-0',
+      title: '人气Top',
+      desc: '2022拿铁冠军',
+      originalPrice: 32,
+      discountPrice: 28,
+      handPrice: 15,
+      imageUrl: coffeePng,
+    },
+    {
+      id: '0-1',
+
+      title: '人气Top',
+      desc: '2022拿铁冠军',
+      originalPrice: 32,
+      discountPrice: 28,
+      handPrice: 15,
+      imageUrl: coffeePng,
+    },
+    {
+      id: '0-2',
+
+      title: '人气Top',
+      desc: '2022拿铁冠军',
+      originalPrice: 32,
+      discountPrice: 28,
+      handPrice: 15,
+      imageUrl: coffeePng,
+    },
+    {
+      id: '0-3',
+
+      title: '人气Top',
+      desc: '2022拿铁冠军',
+      originalPrice: 32,
+      discountPrice: 28,
+      handPrice: 15,
+      imageUrl: coffeePng,
+    },
+    {
+      id: '0-4',
+      title: '人气Top',
+      desc: '2022拿铁冠军',
+      originalPrice: 32,
+      discountPrice: 28,
+      handPrice: 15,
+      imageUrl: coffeePng,
+    },
+  ],
+  // 生酪拿铁
+  [
+    {
+      id: 1,
+      type: 'card',
+      title: '生酪拿铁',
+      subTitle: '必喝爆款，瑞幸精品',
+    },
+    {
+      id: '1-0',
+
+      title: '生酪拿铁',
+      desc: '2022拿铁冠军',
+      originalPrice: 32,
+      discountPrice: 28,
+      handPrice: 15,
+      imageUrl: coffeePng,
+    },
+    {
+      id: '1-1',
+
+      title: '生酪拿铁',
+      desc: '2022拿铁冠军',
+      originalPrice: 32,
+      discountPrice: 28,
+      handPrice: 15,
+      imageUrl: coffeePng,
+    },
+    {
+      id: '1-2',
+
+      title: '生酪拿铁',
+      desc: '2022拿铁冠军',
+      originalPrice: 32,
+      discountPrice: 28,
+      handPrice: 15,
+      imageUrl: coffeePng,
+    },
+    {
+      id: '1-3',
+
+      title: '生酪拿铁',
+      desc: '2022拿铁冠军',
+      originalPrice: 32,
+      discountPrice: 28,
+      handPrice: 15,
+      imageUrl: coffeePng,
+    },
+    {
+      id: '1-4',
+
+      title: '生酪拿铁',
+      desc: '2022拿铁冠军',
+      originalPrice: 32,
+      discountPrice: 28,
+      handPrice: 15,
+      imageUrl: coffeePng,
+    },
+    {
+      id: '1-5',
+
+      title: '生酪拿铁',
+      desc: '2022拿铁冠军',
+      originalPrice: 32,
+      discountPrice: 28,
+      handPrice: 15,
+      imageUrl: coffeePng,
+    },
+    {
+      id: '1-6',
+
+      title: '生酪拿铁',
+      desc: '2022拿铁冠军',
+      originalPrice: 32,
+      discountPrice: 28,
+      handPrice: 15,
+      imageUrl: coffeePng,
+    },
+  ],
+  // 丝绒拿铁
+  [
+    {
+      id: 2,
+      type: 'card',
+      title: '丝绒拿铁',
+      subTitle: '必喝爆款，瑞幸精品',
+    },
+    {
+      id: '2-0',
+
+      title: '丝绒拿铁',
+      desc: '2022拿铁冠军',
+      originalPrice: 32,
+      discountPrice: 28,
+      handPrice: 15,
+      imageUrl: coffeePng,
+    },
+    {
+      id: '2-1',
+
+      title: '丝绒拿铁',
+      desc: '2022拿铁冠军',
+      originalPrice: 32,
+      discountPrice: 28,
+      handPrice: 15,
+      imageUrl: coffeePng,
+    },
+    {
+      id: '2-2',
+
+      title: '丝绒拿铁',
+      desc: '2022拿铁冠军',
+      originalPrice: 32,
+      discountPrice: 28,
+      handPrice: 15,
+      imageUrl: coffeePng,
+    },
+    {
+      id: '2-3',
+
+      title: '丝绒拿铁',
+      desc: '2022拿铁冠军',
+      originalPrice: 32,
+      discountPrice: 28,
+      handPrice: 15,
+      imageUrl: coffeePng,
+    },
+  ],
 ];
